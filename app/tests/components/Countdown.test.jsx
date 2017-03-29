7d926fd09f28c155359d0cc2f3a187fca55a7fc2
stajud001c <sheik_tajudeen@comcast.com>
2017-03-28 22:05:23 -0600

    Added tests and styles

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -16,10 +16,8 @@ describe('Set Countdown',() => {
   it('Should set state to started and countdown',(done) => {
     var countdown = TestUtils.renderIntoDocument(<Countdown />);
     countdown.handleSetCountdown(10);
-
     expect(countdown.state.count).toBe(10);
     expect(countdown.state.countdownStatus).toBe('started');
-
     setTimeout(() => {
       expect(countdown.state.count).toBe(9);
       done();
@@ -29,13 +27,31 @@ describe('Set Countdown',() => {
   it('Should not countdown to negative value',(done) => {
     var countdown = TestUtils.renderIntoDocument(<Countdown />);
     countdown.handleSetCountdown(1);
-
-    expect(countdown.state.count).toBe(1);
-    expect(countdown.state.countdownStatus).toBe('started');
-
     setTimeout(() => {
       expect(countdown.state.count).toBe(0);
       done();
     },3001);
+  });
+
+  it('Should pause countdown on paused status', (done) => {
+    var countdown = TestUtils.renderIntoDocument(<Countdown />);
+    countdown.handleSetCountdown(3);
+    countdown.handleStatusChange('paused');
+    setTimeout(() => {
+      expect(countdown.state.count).toBe(3);
+      expect(countdown.state.countdownStatus).toBe('paused');
+      done();
+    },1001)
+  });
+
+  it('Should stop countdown on stopped status', (done) => {
+    var countdown = TestUtils.renderIntoDocument(<Countdown />);
+    countdown.handleSetCountdown(3);
+    countdown.handleStatusChange('stopped');
+    setTimeout(() => {
+      expect(countdown.state.count).toBe(0);
+      expect(countdown.state.countdownStatus).toBe('stopped');
+      done();
+    },1001)
   });
 });
